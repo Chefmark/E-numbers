28e3c9679bd704224e052059414f15fd322af07a
Chefmark <markus.v.ekman@gmail.com>
2023-09-07 11:39:36 +0200

    added dataframe and storage in database

--- README.md
+++ README.md
@@ -2,6 +2,5 @@ The following project is a demo-project in building a webscraper.
 
 It will scrape Livsmedelsverket for the data regarding E-numbers.
 I will then create a dataframe to store the information.
+From there I will also store the data as a database.
 The data will be used in follow-up parts.
-
-The plan as it stands is to explore and visualize the data.
--- webscraper.py
+++ webscraper.py
@@ -1,28 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import pandas as pd
+import sqlite3
+import os
 
-# Base-URL
-base_url = 'https://www.livsmedelsverket.se/livsmedel-och-innehall/tillsatser-e-nummer/sok-e-nummer/'
+db_filename = "e_numbers.db"
 
+# get href for all e-numbers
+base_url = 'https://www.livsmedelsverket.se/livsmedel-och-innehall/tillsatser-e-nummer/sok-e-nummer'
 response = requests.get(base_url)
-if response.status_code == 200:
-    soup = BeautifulSoup(response.content, 'html.parser')
-    links = soup.find_all('a', href=re.compile(r'/sok-e-nummer/.*'))
+soup = BeautifulSoup(response.content, "html.parser")
+links = soup.find_all("a", class_="name")
+hrefs = [link["href"] for link in links]
+e_nummer = []
+for href in hrefs:
+    e_nummer.append(href.split("sok-e-nummer")[1])
 
-    
-    for link in links:
-        url = base_url + link['href']
+#Add all hrefs to the base_url
+urls = []
+for value in e_nummer:
+    urls.append('https://www.livsmedelsverket.se/livsmedel-och-innehall/tillsatser-e-nummer/sok-e-nummer'+ value)
 
+dfs =[]
+
+for end in urls:
+    res = requests.get(end)
+    if res.status_code == 200:
+        data_soup = BeautifulSoup(res.content, "html.parser")
+        title = data_soup.find('h1').text.strip()
+        name = title.split('-', 1)[1]
         
-        response = requests.get(url)
-        if response.status_code == 200:
+        body = data_soup.find("div", class_= "enumber-details")
+        if body:
+            information_dict = {}
+            information_dict['titles'] = name
             
-            soup = BeautifulSoup(response.content, 'html.parser')
+            elements = body.find_all(['p', 'h4'])
+            current_h4 = None
 
-            title = soup.find('h1').text.strip()
-            print(title)
+            for element in elements:
+                if element.name == 'h4':
+                    current_h4 = element.text.strip()
+                elif element.name == 'p' and current_h4:
+                    information_dict[current_h4] = element.text.strip()
+                
+            df = pd.DataFrame([information_dict])
+            dfs.append(df)
         else:
-            print('Misslyckades att hämta webbsidan. Statuskod:', response.status_code)
-else:
-    print('Misslyckades att hämta bas-URL:n. Statuskod:', response.status_code)
+          print("kunde inte hämta body.")
+    else:
+        print("Kunde inte hämta titlar")
+
+db_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), db_filename)
+
+result_df = pd.concat(dfs, ignore_index=True)
+
+conn = sqlite3.connect('e_numbers.db')
+result_df.to_sql('E_numbers', conn, if_exists='replace', index=False)
+conn.close()
